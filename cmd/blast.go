6e6d99203803280e7d10015671a00a66d6afcb1d
Philipp Heckel <pheckel@datto.com>
2021-02-16 08:27:15 -0500

    Add progress bar to blast

--- cmd/blast.go
+++ cmd/blast.go
@@ -7,7 +7,6 @@ import (
 	"github.com/urfave/cli/v2"
 	"io"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -28,8 +27,69 @@ var cmdBlast = &cli.Command{
 	},
 }
 
+var spinner = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
+
+type progressBar struct {
+	started time.Time
+	count int
+	size int64
+	rendered time.Time
+	rendercount int64
+	prevlen int
+	mu sync.Mutex
+}
+
+func newProgressBar() *progressBar {
+	return &progressBar{
+		started: time.Now(),
+	}
+}
+
+func (p *progressBar) Add(size int64) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.count++
+	p.size += size
+	if time.Since(p.rendered) > 65 * time.Millisecond {
+		p.render(false)
+	}
+}
+
+func (p *progressBar) Done() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.render(true)
+}
+
+func (p *progressBar) render(done bool) {
+	spin := spinner[p.rendercount%int64(len(spinner))]
+	count := p.count
+	countPerSec := float64(p.count) / time.Since(p.started).Seconds()
+	size := bytesToHuman(p.size)
+	sizePerSec := bytesToHuman(int64(float64(p.size) / time.Since(p.started).Seconds()))
+	if done {
+		line := fmt.Sprintf("\rindexing finished: %d docs (%.2f docs/s), %s (%s/s)", count, countPerSec, size, sizePerSec)
+		fmt.Print(line)
+		if p.prevlen > len(line) {
+			fmt.Print(strings.Repeat(" ", p.prevlen-len(line)))
+		}
+		fmt.Println()
+		p.prevlen = len(line)
+	} else {
+		line := fmt.Sprintf("\r%s indexing: %d docs (%.2f docs/s), %s (%s/s)", spin, count, countPerSec, size, sizePerSec)
+		fmt.Print(line)
+		if p.prevlen > len(line) {
+			fmt.Print(strings.Repeat(" ", p.prevlen-len(line)))
+		}
+		p.prevlen = len(line)
+	}
+	p.rendered = time.Now()
+	p.rendercount++
+}
+
 func execBlast(c *cli.Context) error {
 	rand.Seed(time.Now().UnixNano())
+
 	workers := c.Int("workers")
 	nocreate := c.Bool("nocreate")
 
@@ -47,7 +107,6 @@ func execBlast(c *cli.Context) error {
 	if !scanner.Scan() {
 		return cli.Exit("cannot read mapping from STDIN", 1)
 	}
-
 	rawMapping := scanner.Text()
 	if !nocreate {
 		mapping := gjson.Get(rawMapping, index)
@@ -56,9 +115,12 @@ func execBlast(c *cli.Context) error {
 		if err != nil {
 			return err
 		}
-		_, err = client.Do(req)
+		resp, err := client.Do(req)
 		if err != nil {
 			return err
+		}
+		if resp.StatusCode != 201 && resp.StatusCode != 200 {
+			return cli.Exit(fmt.Sprintf("unexpected response code during index creation: %d", resp.StatusCode), 1)
 		}
 	}
 
@@ -67,22 +129,26 @@ func execBlast(c *cli.Context) error {
 
 	wg := &sync.WaitGroup{}
 	docsChan := make(chan string)
+	progress := newProgressBar()
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go blastWorker(wg, docsChan, client, rootURI)
+		go blastWorker(wg, docsChan, progress, client, rootURI)
 	}
 
-	for scanner.Scan() {
-		docsChan <- scanner.Text()
-	}
+	go func() {
+		for scanner.Scan() {
+			docsChan <- scanner.Text()
+		}
+		close(docsChan)
+	}()
 
-	close(docsChan)
 	wg.Wait()
+	progress.Done()
 
 	return nil
 }
 
-func blastWorker(wg *sync.WaitGroup, docsChan chan string, client *http.Client, rootURI string) {
+func blastWorker(wg *sync.WaitGroup, docsChan chan string, progress *progressBar, client *http.Client, rootURI string) {
 	defer wg.Done()
 	for doc := range docsChan {
 		id := url.QueryEscape(gjson.Get(doc, "_id").String())
@@ -91,20 +157,38 @@ func blastWorker(wg *sync.WaitGroup, docsChan chan string, client *http.Client, 
 		uri := fmt.Sprintf("%s/%s/%s", rootURI, dtype, id)
 		req, err := http.NewRequest("PUT", uri, strings.NewReader(source))
 		if err != nil {
-			panic(err)
+			fmt.Printf("error creating PUT request: %s\n", err.Error())
+			continue
 		}
-
-		response, err := client.Do(req)
+		resp, err := client.Do(req)
 		if err != nil {
-			panic(err)
+			fmt.Printf("PUT failed: %s\n", err.Error())
+			continue
 		}
-
-		if response.Body != nil {
-			io.Copy(ioutil.Discard, response.Body)
-			response.Body.Close()
+		if resp.StatusCode != 201 && resp.StatusCode != 200 {
+			fmt.Printf("PUT returned unexpected response: %d\n", resp.StatusCode)
+			continue
 		}
-
-		log.Printf("resp = %s\n", response.Status)
+		if resp.Body != nil {
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+		progress.Add(int64(len(source)))
 	}
+}
+
+func bytesToHuman(b int64) string {
+	// From: https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
 }
 
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.15
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
-	github.com/mdempsky/gocode v0.0.0-20200405233807-4acdcbdea79d // indirect
-	github.com/motemen/gore v0.5.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/tidwall/gjson v1.6.0
 	github.com/urfave/cli/v2 v2.3.0
 )
